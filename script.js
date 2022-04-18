7856c00f18643ff4b4e2bcf4073d609b2d229f81
Mikasa97V <mrsipro2016@yandex.ru>
2022-04-18 12:50:16 +0300

    init repo

---
+++ script.js
@@ -0,0 +1,18 @@
+// window.onload = function () {
+//     let date = new Date(),
+//         h = date.getHours();
+//         m = date.getMinutes();
+//         s = date.getSeconds();
+//         let dateNow = h + ':' + m + ':' + s;
+//     if (dateNow < '8:00:00' || dateNow > '18:00:00') {
+//         document.getElementsByTagName("body")[0].style.backgroundColor = 'black';
+//         document.getElementsByTagName("body")[0].style.color = '#616063';
+//         let color = 'white';  
+//         let elements = document.getElementsByClassName('wrapFigure');
+//         for (let i = 0; i < elements.length; i += 1) {
+//         if (elements[i]) {
+//             elements[i].style.setProperty('--sq-color', color);
+//         }
+//         }
+//     }
+// }
